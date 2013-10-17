ba940974143ddd98d4c698890c42dfea85e4ebae
Ben Hicks <benkhicks@gmail.com>
2013-10-16 21:14:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VacayMe::Application.config.secret_key_base = '93618d072ee40dcb1a99e02efa49688bc27e6c3c63b9e40a67866863c7a2536376f4013d4bcdc842160728118c30e28fb7d86419f9c93a69179bed587205417b'
