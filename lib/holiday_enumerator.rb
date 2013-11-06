534226a8a21abaaf789b9af8e3c8292d7d4723a6
Ben Hicks <benkhicks@gmail.com>
2013-11-05 21:19:36 -0600

    Add library to parse upcoming holidays

---
+++ lib/holiday_enumerator.rb
@@ -0,0 +1,71 @@
+require 'httparty'
+require 'json'
+
+class HolidayEnumerator
+  include Enumerable
+
+  def each
+    calendar_items.map do |item|
+      yield item
+    end
+  end
+
+  private
+
+  def calendar_items
+    json_response['items'].map do |item|
+      {
+        summary:    item['summary'],
+        start_date: item['start']['date'],
+        etag:       item['etag']
+      }
+    end
+  end
+
+  def response
+    @response ||= HTTParty.get GoogleCalendarRequestString.new
+  end
+
+  def json_response
+    @json_response ||= JSON.parse(response.body)
+  end
+
+  class GoogleCalendarRequestString
+    attr_reader :id, :params
+
+    def initialize
+      @id     = 'usa__en@holiday.calendar.google.com'
+      @params = Params.new(google_access_key)
+    end
+
+    def to_str
+      "https://www.googleapis.com/calendar/v3/calendars/#{id}/events?#{params}"
+    end
+
+    private
+
+    def google_access_key
+      ENV.fetch('GOOGLE_ACCESS_KEY') { raise ArgumentError, 'A google access key is required' }
+    end
+
+    class Params
+      attr_reader :key, :time_min, :time_max
+
+      def initialize(key, time_options = {})
+        @key      = key
+        @time_min = time_options.fetch('start') { DateTime.now.to_s }
+        @time_max = time_options.fetch('end') { (DateTime.now + 1.year).to_s }
+      end
+
+      def to_s
+        {
+          key:          key,
+          orderBy:      'startTime',
+          singleEvents: true,
+          timeMin:      time_min,
+          timeMax:      time_max
+        }.to_param
+      end
+    end
+  end
+end
