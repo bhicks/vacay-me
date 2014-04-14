04a49636a2cf75f05219e8604c02f2ab455ce3bc
Ben Hicks <benkhicks@gmail.com>
2014-04-13 23:07:39 -0500

    Remove expired holidays

--- lib/holiday_cache.rb
+++ lib/holiday_cache.rb
@@ -1,7 +1,15 @@
 class HolidayCache
+  KEY ||= 'vacay_me::holidays'
+
   def store
+    yesterday = Time.now - 1.day
+    previous_holidays = Redis.current.zrangebyscore(KEY, 0, yesterday)
+    previous_holidays.each do |previous_holiday|
+      Redis.current.zrem KEY, previous_holiday
+    end
+
     holidays.each do |holiday|
-      Redis.current.zadd 'vacay_me::holidays', holiday[:start_date].to_time.to_i, holiday
+      Redis.current.zadd KEY, holiday[:start_date].to_time.to_i, holiday
     end
   end
 
