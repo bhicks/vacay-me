a2b9f154356684a545924bf08b1231bef5fbfafc
Ben Hicks <benkhicks@gmail.com>
2014-04-17 20:44:32 -0500

    National holiday serializer
    Just using an object for now, not sure if we need active model
    serializers or not.

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,12 +1,20 @@
 class HolidaysController < ApplicationController
   def index
     # TODO: read from redis cache
-    holidays = HolidayList.list
-    holidays.map! do |holiday|
-      holiday[:name] = holiday[:summary]
-      holiday[:date] = holiday[:start_date].strftime("%m/%d/%Y")
-      holiday
-    end
     render json: holidays, status: :ok
+  end
+
+  private
+
+  def holidays
+    HolidayList.list.map {|h| Holiday.new h}
+  end
+
+
+  class Holiday
+    def initialize(holiday_json)
+      @name = holiday_json[:summary]
+      @date = holiday_json[:start_date].strftime("%m/%d/%Y")
+    end
   end
 end
